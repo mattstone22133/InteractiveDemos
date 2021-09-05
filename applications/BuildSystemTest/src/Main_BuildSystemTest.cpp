538148d59085b206c518dc9acc1be6555643325b
matt stone <no email>
2021-09-05 16:52:03 -0400

    refactored and added a few files for testing.

--- applications/BuildSystemTest/src/Main_BuildSystemTest.cpp
+++ applications/BuildSystemTest/src/Main_BuildSystemTest.cpp
@@ -1,10 +1,7 @@
 
 #include "HelloWorld.h"
 #include "Goodbye.h"
-//#include "engine_linker_test.h"
 #include "test/engine_linker_test.h"
-//#include <src/test/engine_linker_test.h>
-//#include"src/test/engine_linker_test.h"
 
 int main()
 {
