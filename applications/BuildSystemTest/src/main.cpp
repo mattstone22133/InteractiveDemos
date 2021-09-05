77736086482d111b19ef92d8b837973faa24bca3
matt stone <no email>
2021-09-05 16:40:29 -0400

    Added engine library. made BuildSystemTest depend on it as a static library.

--- applications/BuildSystemTest/src/main.cpp
+++ applications/BuildSystemTest/src/main.cpp
@@ -1,11 +1,17 @@
 
 #include "HelloWorld.h"
 #include "Goodbye.h"
+//#include "engine_linker_test.h"
+#include "test/engine_linker_test.h"
+//#include <src/test/engine_linker_test.h>
+//#include"src/test/engine_linker_test.h"
 
 int main()
 {
     HelloWorld();
 	Goodbye();
+
+    engine_linker_test();
 
     return 0;
 }
